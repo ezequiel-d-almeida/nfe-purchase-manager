124107eac7a36f8ef14af896491f11f94fbd2061
Ezequiel <almeida.d.ezequiel@gmail.com>
2026-07-26 19:28:34 -0300

    Refatoração do projeto: Alteração na xml_cleaner/main.py

--- xml_cleaner/main.py
+++ xml_cleaner/main.py
@@ -1,20 +1,13 @@
+from pathlib import Path
+
 from select_folder import selecionar_pasta
 from remove_duplicates import remover_duplicados
 from organize_by_date import organizar_por_data
 
-import sys
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(ROOT / "xml_manager"))
-
-from main import main as manager_main
-
-
 OUTPUT_FOLDER = Path(__file__).parent / "output"
 
 
-def main():
+def main() -> Path:
 
     pasta_origem = selecionar_pasta()
 
@@ -25,18 +18,18 @@ def main():
         exist_ok=True
     )
 
-    xmls = remover_duplicados(
+    xml_files = remover_duplicados(
         origem=pasta_origem,
         destino=OUTPUT_FOLDER
     )
 
     print("Organizando XMLs...")
 
-    organizar_por_data(xmls)
+    organizar_por_data(xml_files)
 
-    print(f"XMLs processados: {len(xmls)}")
+    print(f"XMLs processados: {len(xml_files)}")
 
-    manager_main(OUTPUT_FOLDER)
+    return OUTPUT_FOLDER
 
 
 if __name__ == "__main__":
